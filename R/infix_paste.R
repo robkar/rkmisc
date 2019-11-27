a9b9bfab389a28f5ff0c3c35bd4d600aefa46f7a
Robert Karlsson <robkar@gmail.com>
2019-11-27 15:32:43 +0100

    Documented paste operator

--- R/infix_paste.R
+++ R/infix_paste.R
@@ -1,3 +1,13 @@
+#' Paste operator
+#'
+#' An infix operator to concatenate strings
+#'
+#' @param x First string
+#' @param y Second string
+#'
+#' @return x followed by y
+#' @export
+
 `%paste%` <- function(x, y) {
   paste0(x, y)
 }
